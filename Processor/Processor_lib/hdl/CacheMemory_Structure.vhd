00f34d9bc69187193652e0ac81b63910562740c1
Kevin Linger <linger.k@gmail.com>
2013-02-27 23:49:58 -0500

    Cache Structure Update - just needs state machine

--- Processor/Processor_lib/hdl/CacheMemory_Structure.vhd
+++ Processor/Processor_lib/hdl/CacheMemory_Structure.vhd
@@ -2,8 +2,8 @@
 -- VHDL Architecture Processor_lib.CacheMemory.Structure
 --
 -- Created:
---          by - Kevin.UNKNOWN (KEVIN-ULTRABOOK)
---          at - 15:52:40 02/26/2013
+--          by - Kevin.UNKNOWN (KEVIN-PC)
+--          at - 22:26:22 02/27/2013
 --
 -- using Mentor Graphics HDL Designer(TM) 2012.1 (Build 6)
 --
@@ -14,27 +14,21 @@ LIBRARY Processor_lib;
 USE Processor_lib.All;
 
 ENTITY CacheMemory IS
--- Declarations
-Port(clk, we : in std_logic;
+    Port(clk, we : in std_logic;
      din : in std_logic_vector(15 downto 0);
      addr : in std_logic_vector(2 downto 0);
      dout : out std_logic_vector(15 downto 0));
-END CacheMemory ;
+END ENTITY CacheMemory;
 
 --
 ARCHITECTURE Structure OF CacheMemory IS
-  -- SIGNAL addr : std_logic_vector(2 DOWNTO 0);
-  -- SIGNAL clk  : std_logic;
-  -- SIGNAL din  : std_logic_vector(15 DOWNTO 0);
-  -- SIGNAL we   : std_logic;
-  -- SIGNAL dout : std_logic_vector(15 DOWNTO 0);
    COMPONENT mw_ramsp_6bd1b000
       PORT (
          addr : IN     std_logic_vector(2 DOWNTO 0);
          clk  : IN     std_logic;
          din  : IN     std_logic_vector(15 DOWNTO 0);
-         dout : OUT    std_logic_vector(15 DOWNTO 0);
-         we   : IN     std_logic
+         we   : IN     std_logic;
+         dout : OUT    std_logic_vector(15 DOWNTO 0)
       );
    END COMPONENT mw_ramsp_6bd1b000;
    FOR ALL : mw_ramsp_6bd1b000 USE ENTITY Processor_lib.mw_ramsp_6bd1b000;
